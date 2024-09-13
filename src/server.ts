861f1fbcfd2d1a43ce5a0e11dea79f08ca96209b
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-13 01:42:49 -0300

    add: cors + async errors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,33 @@
 // imports
 import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors';
+import cors  from 'cors';
+
+// routes
 import { router } from './routes'
 
 // express
 const app = express()
 app.use(express.json())
 
+// cors
+app.use(cors())
+
 // router
 app.use(router)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error.'
+  })
+})
 
 // server watch
 app.listen(3000, () => console.table(['Listen on http://localhost:3000']))
