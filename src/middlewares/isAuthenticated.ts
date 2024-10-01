1dd97bb203e3500f0254023867302034aea95cfc
Andres Oliveira <andresoliveira@protonmail.com>
2024-10-01 10:52:59 -0300

    add: passing user details into middleware

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { verify } from 'jsonwebtoken'
 
 interface PayLoad{
-  sub: String
+  sub: string,
 }
 
 export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
@@ -22,6 +22,9 @@ export function isAuthenticated(req: Request, res: Response, next: NextFunction)
       token,
       process.env.JWT_SECRET
     ) as PayLoad
+
+    // recover token id and inject into user_id req (typescript)
+    req.user_id = sub
 
     return next()
 
