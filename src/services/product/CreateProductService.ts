7e4f86bb0394ef5bcfc94c2f2874ee8f0fddb632
Andres Oliveira <andresoliveira@protonmail.com>
2024-10-10 10:53:42 -0300

    add: products register w/image

--- src/services/product/CreateProductService.ts
+++ src/services/product/CreateProductService.ts
@@ -11,7 +11,17 @@ interface ProductsRequest {
 class CreateProductService{
   async execute({name, price, description, banner, category_id}: ProductsRequest) {
     
-    return { ok: true }
+    const product = await prismaClient.product.create({
+      data: {
+        name: name,
+        price: price,
+        description: description,
+        banner: banner,
+        category_id: category_id,
+      }
+    })
+
+    return product
   }
 }
 
