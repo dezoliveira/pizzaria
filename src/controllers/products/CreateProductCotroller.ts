8e90ccd1c94778b3b0a42c8134bfba14d6bab5b0
Andres Oliveira <andresoliveira@protonmail.com>
2025-03-20 10:58:37 -0300

    controller update with cloudinary file api

--- src/controllers/products/CreateProductCotroller.ts
+++ src/controllers/products/CreateProductCotroller.ts
@@ -1,28 +1,51 @@
 import { Request, Response } from 'express'
 import { CreateProductService } from '../../services/product/CreateProductService'
+import { UploadedFile } from 'express-fileupload'
+
+import { v2 as cloudinary, UploadApiResponse } from 'cloudinary'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+})
 
 class CreateProductController {
-  async handle(req: Request, res: Response) {
+  async handle(request: Request, response: Response) {
 
-    const { name, price, description, category_id } = req.body
+    const { name, price, description, category_id } = request.body
 
     const createProductService = new CreateProductService()
 
-    if (!req.file) {
-      throw new Error("error upload file")
+    if (!request.files || Object.keys(request.files).length === 0) {
+      throw new Error("error upload file image")
     } else {
+      const file: UploadedFile = request.files['file']
+      
+      const resultFile = await new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream({}, function (error, result) {
+          if (error) {
+            reject(error)
+            return
+          }
 
-      const { originalname, filename: banner } = req.file
-
-      const product = await createProductService.execute({
-        name,
-        price,
-        description,
-        category_id,
-        banner
+          resolve(result)
+        }).end(file.data)
       })
+
+      console.log(resultFile)
+
+      return response.json({})
+
+      // const product = await createProductService.execute({
+      //   name,
+      //   price,
+      //   description,
+      //   category_id,
+      //   banner
+      // })
   
-      return res.json(product)
+      // return response.json(product)
 
     }
 
